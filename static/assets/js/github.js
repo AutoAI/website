fa0f35aa2fc3966f019cd9cdf32b38c7b6fadde4
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-09 19:38:19 -0400

    added list of milestones we care about

--- static/assets/js/github.js
+++ static/assets/js/github.js
@@ -1,3 +1,14 @@
-function getJSON() {
+function getJSONForAllMilestones() {
+    var milestonesToRetrieve[
+        'https://api.github.com/repos/DriveAI/Perception/milestones/1',
+        'https://api.github.com/repos/DriveAI/Perception/milestones/2',
+        'https://api.github.com/repos/DriveAI/Perception/milestones/3',
+        'https://api.github.com/repos/DriveAI/Actuation/milestones/1',
+        'https://api.github.com/repos/DriveAI/PathPlanning/milestones/1',
+        'https://api.github.com/repos/DriveAI/Navigation/milestones/1'
+        ];
+}
+
+function getJSONForMileston() {
     
 }
