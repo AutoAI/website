43de879c4f2b7be4c499213bff85e793298eb3d8
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-09 21:18:26 -0400

    retrieved

--- static/assets/js/github.js
+++ static/assets/js/github.js
@@ -1,22 +1,22 @@
 function getJSONForAllMilestones() {
-    var milestonesToRetrieve = [
+    [
         'https://api.github.com/repos/DriveAI/Perception/milestones/1',
         'https://api.github.com/repos/DriveAI/Perception/milestones/2',
         'https://api.github.com/repos/DriveAI/Perception/milestones/3',
         'https://api.github.com/repos/DriveAI/Actuation/milestones/1',
         'https://api.github.com/repos/DriveAI/PathPlanning/milestones/1',
         'https://api.github.com/repos/DriveAI/Navigation/milestones/1'
-        ];
-
-    var milestoneJSON = [];
-
-    for (var i = 0; i < milestonesToRetrieve.length; i++) {
-        milestoneJSON.push(getJSONForMilestone(milestonesToRetrieve[i]));
-    }
-
-    console.log(milestoneJSON);
+    ].forEach(getJSONForMilestone);
 }
 
-function getJSONForMilestone(url) {
-    return $.get(url);
+function getJSONForMilestone(element, index, array) {
+    $.get(element,  function( data ) {
+        setProgressBarsAndTitles(data);
+    });
+}
+
+function setProgressBarsAndTitles(data){
+    $("progress-bar-parent").append(
+        
+    );
 }
