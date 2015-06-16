d50a26564890d658c94b0f19d7cfe4529c9b67b0
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-16 16:46:37 -0400

    importing stripe

--- process_payment.py
+++ process_payment.py
@@ -1,10 +1,12 @@
 import webapp2
+import stripe
 
-class MainPage(webapp2.RequestHandler):
-    def post(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+class Payment_Processing(webapp2.RequestHandler):
+	def post(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Hello, World!')
+		#token = self.request.get("name")
 
 app = webapp2.WSGIApplication([
-    ('/process_payment', MainPage),
+	('/process_payment', Payment_Processing),
 ], debug=True)
