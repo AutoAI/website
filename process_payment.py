8c9487d431609894d0891c36076c13d5eaf4ae16
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-06-16 19:23:44 -0400

    backend processes payments

--- process_payment.py
+++ process_payment.py
@@ -3,9 +3,21 @@ import stripe
 
 class Payment_Processing(webapp2.RequestHandler):
 	def post(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, World!')
-		#token = self.request.get("name")
+		stripe.api_key = "sk_test_eHsQUr5Aie80HMkJem3HVP1Z"
+		token = self.request.get("stripeToken")
+		amount = self.request.get("amount")
+
+		try:
+			charge = stripe.Charge.create(
+				amount=amount,
+				currency="usd",
+				card=token,
+				description="desc"
+			)
+		except stripe.CardError, e:
+			self.response.status("204")
+			pass
+		self.response.status("400")
 
 app = webapp2.WSGIApplication([
 	('/process_payment', Payment_Processing),
